2031e6bd8c1c6d95d97f07e5b2def68d0bf38614
UjjwalsRepository <99465389+UjjwalsRepository@users.noreply.github.com>
2022-12-27 16:26:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, {useState,useEffect } from "react";
 import AddTodo from "./component/AddTodo";
 import TodoList from "./component/TodoList";
 import axios from "axios";
